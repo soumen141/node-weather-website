3dda8a296ed8ed65ca3f0cdfa852e763612c0fc9
soumen <schakraborty@mobisprint.com>
2021-07-09 00:51:49 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const forecast = require('./utils/forecast')
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 //Defines path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -88,6 +90,6 @@ app.get('*', (req, res) => {
         name: 'Soumen'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 })
